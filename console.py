b9cca2ae601904e964a5f97e60ec906bad99322f
Richouf95 <wourichouf95@gmail.com>
2024-02-09 16:30:26 -0800

    Update console

--- console.py
+++ console.py
@@ -64,6 +64,22 @@ class HBNBCommand(cmd.Cmd):
             "count": self.do_count
         }
 
+        match = re.search(r"\.", arg)
+        if match is None:
+            print("*** Unknown syntax: {}".format(arg))
+            return False
+
+        args_splited = [arg[:match.span()[0]], arg[match.span()[1]:]]
+
+        match = re.search(r"\((.*?)\)", args_splited[1])
+        if match is not None:
+            command = [args_splited[1][:match.span()[0]], match.group()[1:-1]]
+            if command[0] in allow_args.keys():
+                run_command = allow_args[command[0].lower()](
+                        "{} {}".format(args_splited[0], command[1])
+                        )
+                return run_command
+
     def do_create(self, arg):
         """
             Create new instance of a class
@@ -142,7 +158,15 @@ class HBNBCommand(cmd.Cmd):
             Updates an instance based on the class name and id
             by adding or updating attribute
         """
-        command_args = arg.split()
+
+        is_object = re.search(r"\{(.*?)\}", arg)
+        if is_object is not None:
+            arg_split = split(arg[:is_object.span()[0]])
+            command_args = [i.strip(",") for i in arg_split]
+            command_args.append(is_object.group())
+        else:
+            command_args = [i.strip(",") for i in split(arg)]
+
         args_length = len(command_args)
         data = storage.all()
         unauthorized = ["id", "created_at", "updated_at"]
@@ -150,51 +174,68 @@ class HBNBCommand(cmd.Cmd):
         if args_length == 0:
             print("** class name missing **")
             return False
-        elif command_args[0] not in HBNBCommand.list_available_class:
-            print("** class doesn't exist **")
+        elif args_length == 1:
+            if command_args[0] not in HBNBCommand.list_available_class:
+                print("** class doesn't exist **")
+                return False
+            else:
+                print("** instance id missing **")
+                return False
+        elif args_length == 2:
+            chack_intance = "{}.{}".format(command_args[0], command_args[1])
+            if chack_intance not in data.keys():
+                print("** no instance found **")
+            else:
+                print("** attribute name missing **")
             return False
-
-        className = command_args[0]
-
-        if args_length < 2:
-            print("** instance id missing **")
-            return False
-        elif "{}.{}".format(className, command_args[1]) not in data.keys():
-            print("** no instance found **")
-            return False
-
-        instance_id = command_args[1]
-
-        key = "{}.{}".format(className, instance_id)
-
-        if args_length < 3:
-            print("** attribute name missing **")
-            return False
-
-        if args_length < 4 and args_length == 3:
+        elif args_length == 3:
             try:
                 type(eval(command_args[2])) != dict
             except NameError:
                 print("** value missing **")
                 return False
 
-        instance_target = data[key]
+        className = command_args[0]
+        instance_id = command_args[1]
         attribute = command_args[2]
 
         if attribute in unauthorized:
             return False
 
         if args_length == 4:
-            value = command_args[3]
+            instance_target = data["{}.{}".format(className, instance_id)]
             if attribute in instance_target.__class__.__dict__.keys():
-                parse_to = type(instance_target.__class__.__dict__[attribute])
-                instance_target.__dict__[attribute] = parse_to(value)
+                typeOf = type(instance_target.__class__.__dict__[attribute])
+                instance_target.__dict__[attribute] = typeOf(command_args[3])
             else:
-                instance_target.__dict__[attribute] = value
-        elif type(eval(attribute)) == dict:
-            pass
+                instance_target.__dict__[attribute] = command_args[3]
+        else:
+            if type(eval(attribute)) == dict:
+                the_instance = data["{}.{}".format(className, instance_id)]
+                for key, value in eval(attribute).items():
+                    if key in the_instance.__class__.__dict__.keys():
+                        types = {str, int, float}
+                        if type(the_instance.__class__.__dict__[key]) in types:
+                            typeOf = type(the_instance.__class__.__dict__[key])
+                            the_instance.__dict__[key] = typeOf(value)
+                    else:
+                        the_instance.__dict__[key] = value
 
         storage.save()
+
+    def do_count(self, arg):
+        """
+            Count instances of a class
+        """
+        command_args = arg.split()
+        data = storage.all()
+        count = 0
+
+        for x in data.values():
+            if command_args[0] == x.__class__.__name__:
+                count += 1
+
+        print(count)
 
 
 if __name__ == "__main__":
